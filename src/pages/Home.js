95c670ee98e28d2ce6435554fc8fe595b2c92a1d
MyMac <macbook@MacBook.local>
2021-03-17 15:55:45 +0100

    2e commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+//composant Accueil :
+const Home = () => {
+    return (
+        <div className = "home">
+            <h1> Putain d'Accueil </h1>
+        </div>
+    );
+}
+
+export default Home ;
